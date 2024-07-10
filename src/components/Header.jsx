9d0cb33c2d355ecba4b5e84e0394a063e418364b
Hélène Gouguenheim <lngouguenheim@hotmail.fr>
2024-07-10 18:15:38 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles/header.scss";
 
 function Header() {
     return (
-        <>
-            <div className="header">
-                <img className="logo__header"src={logo} alt="logo kasa" />
-                <nav className="navbar__header">
-                    <ul>
-                        <li>
-                            <Link className="navbar__link" to ={"/"}>ACCUEIL</Link>
-                        </li>
-                        <li>
-                            <Link className="navbar__link" to ={"apropos"}>A PROPOS</Link>
-                        </li>
-                    </ul>   
-                </nav>    
-            </div>
-        </>
+        <div className="header">
+            <img className="logo__header" src={logo} alt="logo kasa" />
+            <nav className="navbar__header">
+                <ul>
+                    <li>
+                        <NavLink exact className="navbar__link" to="/" activeClassName="active">ACCUEIL</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className="navbar__link" to="/apropos" activeClassName="active">A PROPOS</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
 export default Header;
-
