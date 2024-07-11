bc1fe0577b5818a1e9bf370ca7a5b93f504bdc48
Hélène Gouguenheim <lngouguenheim@hotmail.fr>
2024-07-11 21:37:37 +0200

    acceuil terminé

--- src/components/Accommodations.jsx
+++ src/components/Accommodations.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Data from "../data.json";
+import { Link } from "react-router-dom";
 import "../styles/accommodations.scss";
-// import { Link } from "react-router-dom";
 
 function Accommodations() {
   return (
     <div className="accommodations-container">
-        {Data.slice(0, 6).map((item) => (
-            <div key={item.id} className="accommodation-card">
-                <h2>{item.title}</h2>
-                <img src={item.cover} alt={item.title} />
-            </div>
-        ))}
+      {Data.map((item) => (
+        <Link 
+          key={item.id}
+          to={`/logement/${item.id}`}
+          className="accommodation-card"
+        >
+          <h2>{item.title}</h2>
+          <img src={item.cover} alt={item.title} />
+        </Link>
+      ))}
     </div>
   );
 }
