dacd770d6da4d34f8efb6c8c669458d0c0803c00
Hélène Gouguenheim <lngouguenheim@hotmail.fr>
2024-09-27 13:12:37 +0200

    deploiement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/<repository-name>/',  // Remplacez <repository-name> par le nom de votre repo GitHub
+  build: {
+    outDir: 'dist',
+  },
 })
