0ebccb0e96a08a6061b8f0f13871b1f2f055e09e
Hélène Gouguenheim <lngouguenheim@hotmail.fr>
2024-09-29 18:32:12 +0200

    deploiement

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repository-name/', // Set this to your GitHub repository name
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-  server: {
-    port: 3000, // Set the desired port for development server
-    open: true, // Automatically open the browser on start
-  }
-});
+})
